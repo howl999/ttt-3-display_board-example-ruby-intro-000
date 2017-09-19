048ca261d234897ea5e0db1ab782677d1e6a055f
howl999 <jc2242@nau.edu>
2017-09-19 23:22:48 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,11 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-def display_board
-  ttt = ["   ", "|",  "   ", "|", "   "]
-  div = "-----------"
-  puts ttt, div, ttt, div, ttt
+it 'prints a 3x3 tic tac toe board' do
+  output = capture_puts{ display_board }
+  output_array = output.split "\n"
+
+    expect(output_array[0]).to include("   |   |   ")
+    expect(output_array[1]).to include("-----------")
+    expect(output_array[2]).to include("   |   |   ")
+    expect(output_array[3]).to include("-----------")
+    expect(output_array[4]).to include("   |   |   ")
 end
